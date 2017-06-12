3951ed8eac9b3b9b28f4f359b1a92ea3379005a2
sunilpaulmathew <sunil.kde@gmail.com>
2017-06-13 02:10:17 +0900

    make.sh:some modifications
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- make.sh
+++ make.sh
@@ -20,7 +20,7 @@ COLOR_GREEN="\033[1;32m"
 COLOR_NEUTRAL="\033[0m"
 PROJECT_ROOT=$PWD
 
-echo -e $COLOR_GREEN"\n Flashable zip generator\n"$COLOR_NEUTRAL
+echo -e $COLOR_GREEN"\n f-zip: commandline flashable zip generator\n"$COLOR_NEUTRAL
 #
 echo -e $COLOR_GREEN"\n (c) sunilpaulmathew@xda-developers.com\n"$COLOR_NEUTRAL
 
@@ -33,8 +33,8 @@ cp META-INF/com/google/android/updater-script $PROJECT_ROOT/backup.sh
 DISPLAY_NAME="Flashing $PROJECT_NAME $PROJECT_VERSION"
 sed -i "s;###Flashing###;${DISPLAY_NAME};" META-INF/com/google/android/updater-script;
 sed -i "s;###copyright###;(c) ${COPYRIGHT};" META-INF/com/google/android/updater-script;
-sed -i "s;# priv-app;set_perm(1000, 1000, 0755, /system/priv-app/${PRIV_APP}/);" META-INF/com/google/android/updater-script;
-sed -i "s;# privapk;set_perm(1000, 1000, 0644, /system/priv-app/${PRIV_APP}/*);" META-INF/com/google/android/updater-script;
+sed -i "s;# priv-app;set_perm(1000, 1000, 0755, "/system/priv-app/${PRIV_APP}";" META-INF/com/google/android/updater-script;
+sed -i "s;# privapk;set_perm(1000, 1000, 0644, "/system/priv-app/${PRIV_APP}/*";" META-INF/com/google/android/updater-script;
 
 # generating recovery flashable zip file
 
@@ -43,3 +43,5 @@ zip -r9 --exclude=*.sh* --exclude=*.git* --exclude=*README* $PROJECT_NAME.zip *
 # restoring original files
 
 mv $PROJECT_ROOT/backup.sh META-INF/com/google/android/updater-script
+
+echo -e $COLOR_GREEN"\n everything done\n"$COLOR_NEUTRA
