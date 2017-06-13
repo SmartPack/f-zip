b0c53043e4e169739c30f24541ee3f75848608a9
sunilpaulmathew <sunil.kde@gmail.com>
2017-06-14 00:59:43 +0900

    fzip.sh: adding more app/priv app support (up to 5 each will be flashable at a time)
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- fzip.sh
+++ fzip.sh
@@ -14,8 +14,16 @@ PROJECT_VERSION=""
 COPYRIGHT=""
 
 # Please provide the exact name (should be case sensitive)
-APP=""
-PRIV_APP=""
+APP1=""
+APP2=""
+APP3=""
+APP4=""
+APP5=""
+PRIV_APP1=""
+PRIV_APP2=""
+PRIV_APP3=""
+PRIV_APP4=""
+PRIV_APP5=""
 
 #
 # Please do not touch unless you know what you are doing... 
@@ -41,25 +49,105 @@ DISPLAY_NAME="Flashing $PROJECT_NAME version $PROJECT_VERSION"
 sed -i "s;###Flashing###;${DISPLAY_NAME};" META-INF/com/google/android/updater-script;
 sed -i "s;###copyright###;(c) ${COPYRIGHT};" META-INF/com/google/android/updater-script;
 
-#  app
+#  app1
 
-if [ -z "$APP" ]; then
-	sed -i "s;set_perm_app;# set_perm;" META-INF/com/google/android/updater-script;		
+if [ -z "$APP1" ]; then
+	sed -i "s;set_perm_app1;# set_perm;" META-INF/com/google/android/updater-script;		
 fi
 
-sed -i "s;set_perm_app;set_perm;" META-INF/com/google/android/updater-script;
-sed -i "s;main-app;/system/app/${APP};" META-INF/com/google/android/updater-script;
-sed -i "s;main-apk;/system/app/${APP}/*;" META-INF/com/google/android/updater-script;
+sed -i "s;set_perm_app1;set_perm;" META-INF/com/google/android/updater-script;
+sed -i "s;main-app1;/system/app/${APP1};" META-INF/com/google/android/updater-script;
+sed -i "s;main-apk1;/system/app/${APP1}/*;" META-INF/com/google/android/updater-script;
 
-# priv-app
+#  app2
 
-if [ -z "$PRIV_APP" ]; then
-	sed -i "s;set_perm_priv;# set_perm;" META-INF/com/google/android/updater-script;		
+if [ -z "$APP2" ]; then
+	sed -i "s;set_perm_app2;# set_perm;" META-INF/com/google/android/updater-script;		
 fi
 
-sed -i "s;set_perm_priv;set_perm;" META-INF/com/google/android/updater-script;
-sed -i "s;priv-app;/system/priv-app/${PRIV_APP};" META-INF/com/google/android/updater-script;
-sed -i "s;priv-apk;/system/priv-app/${PRIV_APP}/*;" META-INF/com/google/android/updater-script;
+sed -i "s;set_perm_app2;set_perm;" META-INF/com/google/android/updater-script;
+sed -i "s;main-app2;/system/app/${APP2};" META-INF/com/google/android/updater-script;
+sed -i "s;main-apk2;/system/app/${APP2}/*;" META-INF/com/google/android/updater-script;
+
+#  app3
+
+if [ -z "$APP3" ]; then
+	sed -i "s;set_perm_app3;# set_perm;" META-INF/com/google/android/updater-script;		
+fi
+
+sed -i "s;set_perm_app3;set_perm;" META-INF/com/google/android/updater-script;
+sed -i "s;main-app3;/system/app/${APP3};" META-INF/com/google/android/updater-script;
+sed -i "s;main-apk3;/system/app/${APP3}/*;" META-INF/com/google/android/updater-script;
+
+#  app4
+
+if [ -z "$APP4" ]; then
+	sed -i "s;set_perm_app4;# set_perm;" META-INF/com/google/android/updater-script;		
+fi
+
+sed -i "s;set_perm_app4;set_perm;" META-INF/com/google/android/updater-script;
+sed -i "s;main-app4;/system/app/${APP4};" META-INF/com/google/android/updater-script;
+sed -i "s;main-apk4;/system/app/${APP4}/*;" META-INF/com/google/android/updater-script;
+
+#  app5
+
+if [ -z "$APP5" ]; then
+	sed -i "s;set_perm_app5;# set_perm;" META-INF/com/google/android/updater-script;		
+fi
+
+sed -i "s;set_perm_app5;set_perm;" META-INF/com/google/android/updater-script;
+sed -i "s;main-app5;/system/app/${APP5};" META-INF/com/google/android/updater-script;
+sed -i "s;main-apk5;/system/app/${APP5}/*;" META-INF/com/google/android/updater-script;
+
+# priv-app1
+
+if [ -z "$PRIV_APP1" ]; then
+	sed -i "s;set_perm_priv1;# set_perm;" META-INF/com/google/android/updater-script;		
+fi
+
+sed -i "s;set_perm_priv1;set_perm;" META-INF/com/google/android/updater-script;
+sed -i "s;priv-app1;/system/priv-app/${PRIV_APP1};" META-INF/com/google/android/updater-script;
+sed -i "s;priv-apk1;/system/priv-app/${PRIV_APP1}/*;" META-INF/com/google/android/updater-script;
+
+# priv-app2
+
+if [ -z "$PRIV_APP2" ]; then
+	sed -i "s;set_perm_priv2;# set_perm;" META-INF/com/google/android/updater-script;		
+fi
+
+sed -i "s;set_perm_priv2;set_perm;" META-INF/com/google/android/updater-script;
+sed -i "s;priv-app2;/system/priv-app/${PRIV_APP2};" META-INF/com/google/android/updater-script;
+sed -i "s;priv-apk2;/system/priv-app/${PRIV_APP2}/*;" META-INF/com/google/android/updater-script;
+
+# priv-app3
+
+if [ -z "$PRIV_APP3" ]; then
+	sed -i "s;set_perm_priv3;# set_perm;" META-INF/com/google/android/updater-script;		
+fi
+
+sed -i "s;set_perm_priv3;set_perm;" META-INF/com/google/android/updater-script;
+sed -i "s;priv-app3;/system/priv-app/${PRIV_APP3};" META-INF/com/google/android/updater-script;
+sed -i "s;priv-apk3;/system/priv-app/${PRIV_APP3}/*;" META-INF/com/google/android/updater-script;
+
+# priv-app4
+
+if [ -z "$PRIV_APP4" ]; then
+	sed -i "s;set_perm_priv4;# set_perm;" META-INF/com/google/android/updater-script;		
+fi
+
+sed -i "s;set_perm_priv4;set_perm;" META-INF/com/google/android/updater-script;
+sed -i "s;priv-app4;/system/priv-app/${PRIV_APP4};" META-INF/com/google/android/updater-script;
+sed -i "s;priv-apk4;/system/priv-app/${PRIV_APP4}/*;" META-INF/com/google/android/updater-script;
+
+# priv-app5
+
+if [ -z "$PRIV_APP5" ]; then
+	sed -i "s;set_perm_priv5;# set_perm;" META-INF/com/google/android/updater-script;		
+fi
+
+sed -i "s;set_perm_priv5;set_perm;" META-INF/com/google/android/updater-script;
+sed -i "s;priv-app5;/system/priv-app/${PRIV_APP5};" META-INF/com/google/android/updater-script;
+sed -i "s;priv-apk5;/system/priv-app/${PRIV_APP5}/*;" META-INF/com/google/android/updater-script;
 
 # generating recovery flashable zip file
 
